e60860dc71ff17c6beb3ad35ca1f1c69d1827e04
tawnynichols <65970072+tawnynichols@users.noreply.github.com>
2020-10-12 15:17:58 -0700

    updating encounters with hospitals

--- LicDataSet/app.py
+++ LicDataSet/app.py
@@ -28,6 +28,8 @@ Base.prepare(engine, reflect=True)
 # Save reference to the table
 Hospitals = Base.classes.hospitals
 Licensed  = Base.classes.LosAnglesCountyLicData
+Encounters = Base.classes.encounters
+Hospitals_Encounters = Base.classes.hospitals_avg_encounters
 
 #################################################
 # Flask Setup
@@ -47,6 +49,8 @@ def welcome():
         f"Available Routes:<br/>"
         f"/api/v1.0/hospitals<br/>"
         f"/api/v1.0/facilities<br/>"
+        f"/api/v1.0/encounters<br/>"
+        f"/api/v1.0/hospitals&encounters<br/>"
     )
 
 
@@ -64,23 +68,56 @@ def hospitals():
 
     """Return a list of dates for each prcp value"""
     # Query all dates and tobs
-    results = session.query(Hospitals.OSHPD_ID, Hospitals.LATITUDE, Hospitals.LONGITUDE).\
+    results = session.query(Hospitals.OSHPD_ID, Hospitals.LATITUDE, Hospitals.LONGITUDE, Hospitals.FACILITY_NAME, Hospitals.DBA_ADDRESS1,Hospitals.DBA_CITY, Hospitals.DBA_ZIP_CODE).\
         order_by(Hospitals.OSHPD_ID).all()
 
     session.close()
 
     # Create a dictionary from the row data and append to a list of all_hospitals
     all_hospitals = []
-    for id, lat, lon in results:
+    for id, lat, lon, name, address, city, zip in results:
         hoptial_dict = {}
         hoptial_dict["ID"] = id
         hoptial_dict["LATITUDE"] = lat
         hoptial_dict["LONGITUDE"] = lon
+        hoptial_dict["FACILITY_NAME"] = name
+        hoptial_dict["DBA_ADDRESS1"] = address
+        hoptial_dict["DBA_CITY"] = city
+        hoptial_dict["DBA_ZIP_CODE"] = zip
         all_hospitals.append(hoptial_dict)
 
     return jsonify(all_hospitals)
 
 
+@app.route("/api/v1.0/hospitals&encounters")
+def hos_enc():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of dates for each prcp value"""
+    # Query all dates and tobs
+    results = session.query(Hospitals_Encounters.OSHPD_ID, Hospitals_Encounters.LATITUDE, Hospitals_Encounters.LONGITUDE, Hospitals_Encounters.FAC_NAME, Hospitals_Encounters.DBA_ADDRESS1,Hospitals_Encounters.DBA_CITY, Hospitals_Encounters.DBA_ZIP_CODE, Hospitals_Encounters.TOTAL_NUMBER_BEDS, Hospitals_Encounters.AvgAdmits, Hospitals_Encounters.AvgVisits).\
+        order_by(Hospitals_Encounters.OSHPD_ID).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_hospitals
+    all_hospitals = []
+    for id, lat, lon, name, address, city, zip, beds, admits, visits in results:
+        hoptial_dict = {}
+        hoptial_dict["ID"] = id
+        hoptial_dict["LATITUDE"] = lat
+        hoptial_dict["LONGITUDE"] = lon
+        hoptial_dict["FACILITY_NAME"] = name
+        hoptial_dict["DBA_ADDRESS1"] = address
+        hoptial_dict["DBA_CITY"] = city
+        hoptial_dict["DBA_ZIP_CODE"] = zip
+        hoptial_dict["Total_beds"] = beds
+        hoptial_dict["AvgAdmits"] = admits
+        hoptial_dict["AvgVisits"] = visits
+        all_hospitals.append(hoptial_dict)
+
+    return jsonify(all_hospitals)
 
 @app.route("/api/v1.0/facilities")
 def facilities():
@@ -107,6 +144,32 @@ def facilities():
         all_facilities.append(facilities_dict)
 
     return jsonify(all_facilities)
+
+
+@app.route("/api/v1.0/encounters")
+def encounters():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of dates for each prcp value"""
+    # Query all dates and tobs
+    results = session.query(Encounters.oshpd_id, Encounters.facility_name, Encounters.year, Encounters.type,  Encounters.count).\
+        order_by(Encounters.year).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_facilities
+    all_encounters = []
+    for id, name, year, type, count in results:
+        encounters_dict = {}
+        encounters_dict["oshpd_id"] = id
+        encounters_dict["facility_name"] = name
+        encounters_dict["Year"] = year
+        encounters_dict["Type"] = type
+        encounters_dict["Count"] = count
+        all_encounters.append(encounters_dict)
+
+    return jsonify(all_encounters)
 
 
 #################################################
