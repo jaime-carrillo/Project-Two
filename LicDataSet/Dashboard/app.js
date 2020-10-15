973d0041b0d0f65875101053df865c7ffbf36006
tawnynichols <65970072+tawnynichols@users.noreply.github.com>
2020-10-14 18:33:47 -0700

    updated dashboard

--- LicDataSet/Dashboard/app.js
+++ LicDataSet/Dashboard/app.js
@@ -12,7 +12,6 @@ console.log(url)
 // })
 
 
-
 function getValues(id) {
 
     // Fetch the JSON data and console log it
@@ -24,16 +23,20 @@ function getValues(id) {
 
         labels = []
         values = []
-        beds = []
+        population = []
+        foods = []
         importedData.forEach(function(obj) {
             var label = obj.GEONAME
             labels.push(label)
 
-            var value = obj.Median_incoms
+            var value = obj.Health_index_rank
             values.push(value)
 
-            var bed = obj.Pop_Tot
-            beds.push(bed)
+            var pop = obj.Prop_65y_rank
+            population.push(pop)
+
+            var food = obj.Food_insecurity_rank
+            foods.push(food)
 
         });
 
@@ -45,15 +48,43 @@ function getValues(id) {
 
         // Create your trace.
         var trace = {
-            x: values,
-            y: labels,
-            text: labels,
-            type: "bar",
-            orientation: "h"
+            x: labels,
+            y: values,
+            name: 'Health Index',
+            type: 'bar',
+            text: values.map(String),
+            textposition: 'auto',
+            hoverinfo: 'none',
+            marker: {
+                color: 'rgb(158,202,225)',
+                opacity: 0.7,
+                line: {
+                    color: 'rgb(8,48,107)',
+                    width: 1.5
+                }
+            }
+        };
+
+        var trace2 = {
+            x: labels,
+            y: foods,
+            name: 'Food Insecurity',
+            type: 'bar',
+            text: foods.map(String),
+            textposition: 'auto',
+            hoverinfo: 'none',
+            marker: {
+                color: 'orange',
+                opacity: 0.5,
+                line: {
+                    color: 'rgb(8,48,107)',
+                    width: 1.5
+                }
+            }
         };
 
         // Create the data array for our plot
-        var data = [trace];
+        var data = [trace, trace2];
 
         // Define the plot layout
         var layout = {
@@ -61,36 +92,35 @@ function getValues(id) {
             yaxis: {
                 tickmode: "linear",
             },
-            height: 1200,
-            width: 800
+            height: 500,
+            width: 1000
         };
 
         // Plot the chart to a div tag with id "bar"
         Plotly.newPlot("bar", data, layout);
 
-        // Create data array for bubble chart
-        var trace1 = {
-            x: values,
-            y: beds,
-            mode: "markers",
-            marker: {
-                size: beds //,
-                    //color: labels
-            },
-            text: importedData[0].GEONAME
+        // // Create data array for bubble chart
+        // var trace1 = {
+        //     x: values,
+        //     y: foods,
+        //     text: foods.map(String),
+        //     mode: 'markers',
+        //     marker: {
+        //         color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)', 'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+        //         size: population
+        //     }
+        // };
 
-        };
+        // var data1 = [trace1];
+        // //Creat layout of bubble chart
+        // var layout1 = {
+        //     xaxis: { title: "Title" },
+        //     height: 800,
+        //     width: 1200
+        // };
 
-        var data1 = [trace1];
-        //Creat layout of bubble chart
-        var layout1 = {
-            xaxis: { title: "OSHPID" },
-            height: 800,
-            width: 1200
-        };
-
-        //Plot bubble chart
-        Plotly.newPlot("bubble", data1, layout1);
+        // //Plot bubble chart
+        // Plotly.newPlot("bubble", data1, layout1);
 
     })
 
@@ -112,20 +142,20 @@ function getData(id) {
             profile_dict = {}
             profile_dict["GEONAME"] = obj.GEONAME
             profile_dict["Pop_Tot"] = obj.Pop_Tot
-                // profile_dict["Pop_Tot_Per"] = totP
-                // profile_dict["Prop_65y"] = age
-                // profile_dict["Prop_65y_rank"] = age_r
+            profile_dict["Pop_Tot_Per"] = obj.Pop_Tot_Per
+            profile_dict["Prop_65y"] = obj.Prop_65y
+                //profile_dict["Prop_65y_rank"] = obj.Prop_65y_rank
                 // profile_dict["Poverty"] = poverty
                 // profile_dict["Poverty_rank"] = povertyR
-                // profile_dict["Median_incoms"] = mi
+            profile_dict["Median_incoms"] = obj.Median_incoms
                 // profile_dict["MI_rank"] = mirank
                 // profile_dict["Farmers_market"] = market
                 // profile_dict["Farmers_market_rank"] = marketrank
-                // profile_dict["Food_insecurity"] = food
+            profile_dict["Food_insecurity"] = obj.Food_insecurity
                 // profile_dict["Food_insecurity_rank"] = foorank
                 // profile_dict["School_Meals"] = meal
                 // profile_dict["School_Meals_rank"] = mealrank
-                // profile_dict["Health_index"] = idx
+            profile_dict["Health_index"] = obj.Health_index
                 // profile_dict["Health_index_rank"] = idxrank
 
             demoData.push(profile_dict)
@@ -148,102 +178,103 @@ function getData(id) {
         demoInfo.html("");
 
         // get demographic data for the id and append to panel
-        // Object.values(info).forEach((key) => {
-        //     demoInfo.append("h5").text(info[0].GEONAME + ": " + info[1] + "\n")
-        // });
+        // info.forEach((x) => {
+        //     demoInfo.append("h5").text(x[0])
+
+        demoInfo.append("h4").text(info[0].GEONAME + "Total Population: " + info[0].Pop_Tot + " (" + info[0].Pop_Tot_Per + ")" + "\n")
+            .append("h5").text("Over 65: " + info[0].Prop_65y)
+            .append("h5").text("Median Income: " + info[0].Median_incoms)
+            .append("h5").text("Food Insecurity: " + info[0].Food_insecurity)
+            .append("h5").text("Health Index: " + info[0].Health_index)
+            //     })
+            // }
 
 
-        demoInfo.append("h5").text("Name: " + info[0].GEONAME + "\n" + "Total Population: " + info[0].Pop_Tot + "\n")
+        // Enter a speed between 0 and 180
+        var level = info[0].Health_index * 1.75 //info.wfreq;
 
-    })
+        // Trig to calc meter point
+        var degrees = 180 - level,
+            radius = .5;
+        var radians = degrees * Math.PI / 180;
+        var x = radius * Math.cos(radians);
+        var y = radius * Math.sin(radians);
+        var path1 = (degrees < 45 || degrees > 135) ? 'M -0.0 -0.025 L 0.0 0.025 L ' : 'M -0.025 -0.0 L 0.025 0.0 L ';
+        // Path: may have to change to create a better triangle
+        var mainPath = path1,
+            pathX = String(x),
+            space = ' ',
+            pathY = String(y),
+            pathEnd = ' Z';
+        var path = mainPath.concat(pathX, space, pathY, pathEnd);
+
+        var data2 = [{
+                type: 'scatter',
+                x: [0],
+                y: [0],
+                marker: { size: 14, color: '850000' },
+                showlegend: false,
+                name: 'speed',
+                text: level,
+                hoverinfo: 'text+name'
+            },
+            {
+                values: [1, 1, 1, 1, 1, 1, 6],
+                rotation: 90,
+                text: ['100', '80', '60', '40', '20', '0', ''],
+                textinfo: 'text',
+                textposition: 'inside',
+                marker: {
+                    colors: ['#699c2b', '#73a842',
+                        '#7fb356', '#8ebe6b', '#9fc97f', '#b2d494', '#c5dea8', '#dae7bd', '#f1f1d2', '#f1f1d2',
+                        'rgba(0, 0, 0, 0)'
+                    ]
+                },
+                hoverinfo: 'label',
+                hole: .5,
+                type: 'pie',
+                showlegend: false
+            }
+        ];
+
+        var layout2 = {
+            shapes: [{
+                type: 'path',
+                path: path,
+                fillcolor: '850000',
+                line: {
+                    color: '850000'
+                }
+            }],
+            title: "Belly Button Washing Frequency",
+            subtitle: 'Plot Subtitle',
+            height: 400,
+            width: 400,
+            xaxis: {
+                zeroline: false,
+                showticklabels: false,
+                showgrid: false,
+                range: [-1, 1],
+                titlefont: {
+                    title: 'x Axis',
+                    family: 'Courier New, monospace',
+                    size: 18,
+                    color: '#7f7f7f'
+                }
+            },
+            yaxis: {
+                zeroline: false,
+                showticklabels: false,
+                showgrid: false,
+                range: [-1, 1]
+            }
+        };
+
+        //Plot gauge chart
+        Plotly.newPlot('gauge1', data2, layout2);
+    });
+
 }
-
-
-//         // Enter a speed between 0 and 180
-//         var level = info.wfreq * 20.5 //info.wfreq;
-
-//         // Trig to calc meter point
-//         var degrees = 180 - level,
-//             radius = .5;
-//         var radians = degrees * Math.PI / 180;
-//         var x = radius * Math.cos(radians);
-//         var y = radius * Math.sin(radians);
-//         var path1 = (degrees < 45 || degrees > 135) ? 'M -0.0 -0.025 L 0.0 0.025 L ' : 'M -0.025 -0.0 L 0.025 0.0 L ';
-//         // Path: may have to change to create a better triangle
-//         var mainPath = path1,
-//             pathX = String(x),
-//             space = ' ',
-//             pathY = String(y),
-//             pathEnd = ' Z';
-//         var path = mainPath.concat(pathX, space, pathY, pathEnd);
-
-//         var data2 = [{
-//                 type: 'scatter',
-//                 x: [0],
-//                 y: [0],
-//                 marker: { size: 14, color: '850000' },
-//                 showlegend: false,
-//                 name: 'speed',
-//                 text: level,
-//                 hoverinfo: 'text+name'
-//             },
-//             {
-//                 values: [1, 1, 1, 1, 1, 1, 1, 1, 1, 9],
-//                 rotation: 90,
-//                 text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
-//                 textinfo: 'text',
-//                 textposition: 'inside',
-//                 marker: {
-//                     colors: ['#699c2b', '#73a842',
-//                         '#7fb356', '#8ebe6b', '#9fc97f', '#b2d494', '#c5dea8', '#dae7bd', '#f1f1d2',
-//                         'rgba(0, 0, 0, 0)'
-//                     ]
-//                 },
-//                 hoverinfo: 'label',
-//                 hole: .5,
-//                 type: 'pie',
-//                 showlegend: false
-//             }
-//         ];
-
-//         var layout2 = {
-//             shapes: [{
-//                 type: 'path',
-//                 path: path,
-//                 fillcolor: '850000',
-//                 line: {
-//                     color: '850000'
-//                 }
-//             }],
-//             title: "Belly Button Washing Frequency",
-//             subtitle: 'Plot Subtitle',
-//             height: 400,
-//             width: 400,
-//             xaxis: {
-//                 zeroline: false,
-//                 showticklabels: false,
-//                 showgrid: false,
-//                 range: [-1, 1],
-//                 titlefont: {
-//                     title: 'x Axis',
-//                     family: 'Courier New, monospace',
-//                     size: 18,
-//                     color: '#7f7f7f'
-//                 }
-//             },
-//             yaxis: {
-//                 zeroline: false,
-//                 showticklabels: false,
-//                 showgrid: false,
-//                 range: [-1, 1]
-//             }
-//         };
-
-//         //Plot gauge chart
-//         Plotly.newPlot('gauge', data2, layout2);
-//     });
-
-// }
 
 // create  function for change event
 function optionChanged(id) {
