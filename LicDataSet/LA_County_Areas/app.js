c61102ecf3ec1bff5e77d5b9b2287a2bc449584b
tawnynichols <65970072+tawnynichols@users.noreply.github.com>
2020-10-14 22:18:36 -0700

    combined map and dashbaord in webpage

---
+++ LicDataSet/LA_County_Areas/app.js
@@ -0,0 +1,416 @@
+// Store API query variables
+var baseURL = "http://127.0.0.1:5000";
+var option = "/api/v1.0/profiles";
+
+// Assemble API query URL
+var url = baseURL + option
+console.log(url)
+
+function getValues(id) {
+
+    // Fetch the JSON data and console log it
+    d3.json(url).then(importedData => {
+        // console.log(importedData.names);
+        // console.log(importedData)
+        // console.log(importedData.samples.otu_ids)
+        // console.log(importedData.samples[0].sample_values)
+
+        labels = []
+        values = []
+        population = []
+        foods = []
+        importedData.forEach(function(obj) {
+            var label = obj.GEONAME
+            labels.push(label)
+
+            var value = obj.Health_index_rank
+            values.push(value)
+
+            var pop = obj.Prop_65y_rank
+            population.push(pop)
+
+            var food = obj.Food_insecurity_rank
+            foods.push(food)
+
+        });
+
+        //#############################################################
+        // Create bar chart
+        //#############################################################
+
+        // Create your trace.
+        var trace = {
+            x: labels,
+            y: values,
+            name: 'Health Index',
+            type: 'bar',
+            text: values.map(String),
+            textposition: 'auto',
+            hoverinfo: 'none',
+            marker: {
+                color: 'rgb(158,202,225)',
+                opacity: 0.7,
+                line: {
+                    color: 'rgb(8,48,107)',
+                    width: 1.5
+                }
+            }
+        };
+
+        var trace2 = {
+            x: labels,
+            y: foods,
+            name: 'Food Insecurity',
+            type: 'bar',
+            text: foods.map(String),
+            textposition: 'auto',
+            hoverinfo: 'none',
+            marker: {
+                color: 'orange',
+                opacity: 0.5,
+                line: {
+                    color: 'rgb(8,48,107)',
+                    width: 1.5
+                }
+            }
+        };
+
+        // Create the data array for our plot
+        var data = [trace, trace2];
+
+        // Define the plot layout
+        var layout = {
+            title: "title",
+            yaxis: {
+                tickmode: "linear",
+            },
+            height: 500,
+            width: 1000
+        };
+
+        // Plot the chart to a div tag with id "bar"
+        Plotly.newPlot("bar", data, layout);
+
+        // // Create data array for bubble chart
+        // var trace1 = {
+        //     x: values,
+        //     y: foods,
+        //     text: foods.map(String),
+        //     mode: 'markers',
+        //     marker: {
+        //         color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)', 'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+        //         size: population
+        //     }
+        // };
+
+        // var data1 = [trace1];
+        // //Creat layout of bubble chart
+        // var layout1 = {
+        //     xaxis: { title: "Title" },
+        //     height: 800,
+        //     width: 1200
+        // };
+
+        // //Plot bubble chart
+        // Plotly.newPlot("bubble", data1, layout1);
+
+    })
+
+}
+
+//#############################################################
+// On change to the DOM, call getData()
+//#############################################################
+
+
+function getData(id) {
+    d3.json(url).then((data) => {
+        // console.log(demoData)
+
+        // demoData = data
+        // console.log(data)
+
+        demoData = []
+        console.log(demoData)
+
+        data.forEach(function(obj) {
+            profile_dict = {}
+            profile_dict["GEONAME"] = obj.GEONAME
+            profile_dict["Pop_Tot"] = obj.Pop_Tot
+            profile_dict["Pop_Tot_Per"] = obj.Pop_Tot_Per
+            profile_dict["Prop_65y"] = obj.Prop_65y
+                //profile_dict["Prop_65y_rank"] = obj.Prop_65y_rank
+                // profile_dict["Poverty"] = poverty
+                // profile_dict["Poverty_rank"] = povertyR
+            profile_dict["Median_incoms"] = obj.Median_incoms
+                // profile_dict["MI_rank"] = mirank
+                // profile_dict["Farmers_market"] = market
+                // profile_dict["Farmers_market_rank"] = marketrank
+            profile_dict["Food_insecurity"] = obj.Food_insecurity
+                // profile_dict["Food_insecurity_rank"] = foorank
+                // profile_dict["School_Meals"] = meal
+                // profile_dict["School_Meals_rank"] = mealrank
+            profile_dict["Health_index"] = obj.Health_index
+                // profile_dict["Health_index_rank"] = idxrank
+
+            demoData.push(profile_dict)
+                // console.log(profile_dict)
+        })
+        console.log(id)
+
+        //#############################################################
+        // define variable to filter data
+        //#############################################################
+
+        // var info = demoData.filter(d => d === id)[0];
+        var info = demoData.filter(d => d.GEONAME == id)
+            // console.log(info[0].ID)
+
+        console.log(info[0].GEONAME)
+
+        // select demographic data
+        var demoInfo = d3.select("#sample-metadata");
+
+        //clear demographic info before update
+        demoInfo.html("");
+
+        // get demographic data for the id and append to panel
+        // info.forEach((x) => {
+        //     demoInfo.append("h5").text(x[0])
+
+        demoInfo.append("h4").text(info[0].GEONAME)
+            .append("h4").text("Total Population: " + info[0].Pop_Tot + " (" + info[0].Pop_Tot_Per + ")" + "\n")
+            .append("h6").text("Over 65: " + info[0].Prop_65y)
+            .append("h6").text("Median Income: " + info[0].Median_incoms)
+            .append("h6").text("Food Insecurity: " + info[0].Food_insecurity)
+            .append("h6").text("Health Index: " + info[0].Health_index)
+            //     })
+            // }
+
+        //#############################################################
+        // Gauge for dynamic district
+        //#############################################################
+
+        // Enter a speed between 0 and 180
+        var level = info[0].Health_index * 1.71 //info.wfreq;
+
+        // Trig to calc meter point
+        var degrees = 180 - level,
+            radius = .5;
+        var radians = degrees * Math.PI / 180;
+        var x = radius * Math.cos(radians);
+        var y = radius * Math.sin(radians);
+        var path1 = (degrees < 45 || degrees > 135) ? 'M -0.0 -0.025 L 0.0 0.025 L ' : 'M -0.025 -0.0 L 0.025 0.0 L ';
+        // Path: may have to change to create a better triangle
+        var mainPath = path1,
+            pathX = String(x),
+            space = ' ',
+            pathY = String(y),
+            pathEnd = ' Z';
+        var path = mainPath.concat(pathX, space, pathY, pathEnd);
+
+        var data2 = [{
+                type: 'scatter',
+                x: [0],
+                y: [0],
+                marker: { size: 14, color: '850000' },
+                showlegend: false,
+                name: 'Index',
+                text: info[0].Health_index,
+                hoverinfo: 'text+name'
+            },
+            {
+                values: [1, 1, 1, 1, 1, 1, 6],
+                rotation: 90,
+                text: ['100', '80', '60', '40', '20', '0', ''],
+                textinfo: 'text',
+                textposition: 'inside',
+                marker: {
+                    colors: ['#8ebe6b', '#9fc97f', '#b2d494', '#c5dea8', '#dae7bd', '#f1f1d2',
+                        'rgba(0, 0, 0, 0)'
+                    ]
+                },
+                hoverinfo: 'label',
+                hole: .5,
+                type: 'pie',
+                showlegend: false
+            }
+        ];
+
+        var layout2 = {
+            shapes: [{
+                type: 'path',
+                path: path,
+                fillcolor: 'rgba(0, 0, 0, 0)',
+                line: {
+                    color: '850000'
+                }
+            }],
+            title: info[0].GEONAME,
+            subtitle: 'Plot Subtitle',
+            height: 370,
+            width: 370,
+            xaxis: {
+                zeroline: false,
+                showticklabels: false,
+                showgrid: false,
+                range: [-1, 1],
+                titlefont: {
+                    title: 'x Axis',
+                    family: 'Courier New, monospace',
+                    size: 18,
+                    color: '#7f7f7f'
+                }
+            },
+            yaxis: {
+                zeroline: false,
+                showticklabels: false,
+                showgrid: false,
+                range: [-1, 1]
+            }
+        };
+
+        //Plot gauge chart
+        Plotly.newPlot('gauge1', data2, layout2);
+
+        //#############################################################
+        // Gauge for Los Angles County Average
+        //#############################################################
+        var benchmark = 44.73372093
+            // Enter a speed between 0 and 180
+        var level = benchmark * 1.71 //info.wfreq;
+
+        // Trig to calc meter point
+        var degrees = 180 - level,
+            radius = .5;
+        var radians = degrees * Math.PI / 180;
+        var x = radius * Math.cos(radians);
+        var y = radius * Math.sin(radians);
+        var path1 = (degrees < 45 || degrees > 135) ? 'M -0.0 -0.025 L 0.0 0.025 L ' : 'M -0.025 -0.0 L 0.025 0.0 L ';
+        // Path: may have to change to create a better triangle
+        var mainPath = path1,
+            pathX = String(x),
+            space = ' ',
+            pathY = String(y),
+            pathEnd = ' Z';
+        var path = mainPath.concat(pathX, space, pathY, pathEnd);
+
+        var data2 = [{
+                type: 'scatter',
+                x: [0],
+                y: [0],
+                marker: { size: 14, color: '850000' },
+                showlegend: false,
+                name: 'Index',
+                text: benchmark,
+                hoverinfo: 'text+name'
+            },
+            {
+                values: [1, 1, 1, 1, 1, 1, 6],
+                rotation: 90,
+                text: ['100', '80', '60', '40', '20', '0', ''],
+                textinfo: 'text',
+                textposition: 'inside',
+                marker: {
+                    colors: ['#8ebe6b', '#9fc97f', '#b2d494', '#c5dea8', '#dae7bd', '#f1f1d2',
+                        'rgba(0, 0, 0, 0)'
+                    ]
+                },
+                hoverinfo: 'label',
+                hole: .5,
+                type: 'pie',
+                showlegend: false
+            }
+        ];
+
+        var layout2 = {
+            shapes: [{
+                type: 'path',
+                path: path,
+                fillcolor: 'rgba(0, 0, 0, 0)',
+                line: {
+                    color: '850000'
+                }
+            }],
+            title: 'Los Angles County',
+            subtitle: 'Plot Subtitle',
+            height: 400,
+            width: 400,
+            xaxis: {
+                zeroline: false,
+                showticklabels: false,
+                showgrid: false,
+                range: [-1, 1],
+                titlefont: {
+                    title: 'x Axis',
+                    family: 'Courier New, monospace',
+                    size: 18,
+                    color: '#7f7f7f'
+                }
+            },
+            yaxis: {
+                zeroline: false,
+                showticklabels: false,
+                showgrid: false,
+                range: [-1, 1]
+            }
+        };
+
+        //Plot gauge chart
+        Plotly.newPlot('gauge2', data2, layout2);
+    });
+
+
+
+}
+
+// create  function for change event
+function optionChanged(id) {
+    getValues(id);
+    getData(id);
+}
+
+
+function init() {
+    // Assign the value of the dropdown menu option to a variable
+    var dropdownMenu = d3.select("#selDataset");
+
+    // read the data 
+    d3.json(url).then((data) => {
+
+        names = []
+        data.forEach(function(obj) {
+                var label = obj.GEONAME
+                names.push(label)
+            })
+            // console.log(names)
+
+        //get ids for dropdow
+        names.forEach(function(ID) {
+            dropdownMenu.append("option").text(ID).property("value");
+        })
+
+        // call functions to display plot
+        getValues(names[0]);
+        getData(names[0]);
+
+    })
+}
+
+// function init() {
+
+//     var dropdown = d3.select("#selDataset");
+
+//     d3.json("samples.json").then((data) => {
+
+//         data.names.forEach(function(name) {
+//             dropdown.append("option").text(name).property("value");
+//         });
+
+//         buildPlots(data.names[0]);
+//         demoInfo(data.names[0])
+//     });
+// }
+
+
+init();
---
+++ LicDataSet/LA_County_Areas/static/js/logic.js
@@ -0,0 +1,314 @@
+// Load in geojson data for income
+var geoData = "static/data/Median_Household_Income_2016.geojson";
+
+// Grab data with d3
+
+d3.json(geoData, function(income_data) {
+
+    // Create a new choropleth layer
+    var income = L.choropleth(income_data, {
+
+            // Define what  property in the features to use
+            valueProperty: "MHI2016",
+
+            // Set color scale
+            scale: ["#00c7ff", "#94003a"],
+
+            // Number of breaks in step range
+            steps: 6,
+
+            // q for quartile, e for equidistant, k for k-means
+            mode: "q",
+            style: {
+                // Border color
+                color: "#fff",
+                weight: 1,
+                fillOpacity: 0.9
+            },
+
+            // Binding a pop-up to each layer
+            onEachFeature: function(feature, layer) {
+                layer.bindPopup("Zip Code: " + feature.properties.ZIP + "<br>Median Household Income:<br>" +
+                    "$" + feature.properties.MHI2016);
+            }
+        }) //.addTo(myMap);
+
+    //Set up Service Planning Area boundries
+    var spa_link = "static/data/spa.geojson"
+
+    //set up color function for each area
+    function chooseColor(objectid) {
+        switch (objectid) {
+            case "1":
+                return "orange";
+            case "2":
+                return "yellow";
+            case "3":
+                return "green";
+            case "4":
+                return "purple";
+            case "5":
+                return "green";
+            case "6":
+                return "yellow";
+            case "7":
+                return "orange";
+            case "8":
+                return "purple";
+            default:
+                return "grey";
+        }
+    }
+
+
+    d3.json(spa_link, function(spa_data) {
+        console.log(spa_data)
+        spa = L.geoJson(spa_data, {
+                style: function(feature) {
+                    return {
+                        color: chooseColor(feature.properties.objectid),
+                        //fillColor: "blue", //chooseColor(feature.properties.area),
+                        fillOpacity: .9,
+                        weight: 3
+                    }
+                },
+                onEachFeature: function(feature, layer) {
+                    layer.on({
+                        mouseover: function(event) {
+                            layer = event.target
+                            layer.setStyle({
+                                fillOpacity: .2
+                            });
+                        },
+                        mouseout: function(event) {
+                                layer = event.target
+                                layer.setStyle({
+                                    fillOpacity: .7
+                                })
+                            }
+                            // ,
+                            // click: function(event) {
+                            //     myMap.fitBounds(event.target.getBounds())
+                            // }
+                    });
+                    layer.bindPopup("<h1>" + feature.properties.spa_name + " (" + feature.properties.abbv + ")" + "</h1>")
+                }
+            }) //.addTo(myMap)
+    });
+
+    // Store API query variables
+    var baseURL = "http://127.0.0.1:5000";
+    var option = "/api/v1.0/hospitals";
+
+    // Assemble API query URL
+    var url = baseURL + option
+    console.log(url)
+
+    // Grab the data with d3
+    d3.json(url, function(response) {
+
+        // var parentGroup = L.markerClusterGroup()
+
+        // Create feature group
+        test = L.featureGroup(getArrayOfMarkers())
+
+        // test = L.featureGroup.subGroup(
+        //     parentGroup,
+        //     getArrayOfMarkers()
+        // )
+
+        //Create function to get an array of the lat and lon
+
+        function getArrayOfMarkers() {
+            var result = [];
+            var popup = [];
+
+            for (var i = 0; i < response.length; i += 1) {
+
+                if (location) {
+                    var lat = response[i].LATITUDE;
+                    var lon = response[i].LONGITUDE;
+                    var name = response[i].FACILITY_NAME
+
+                    result.push(L.marker([lat, lon]));
+                    popup.push(name);
+                    //result.push([lat, lon])
+
+                }
+            }
+            return result;
+        }
+
+        //Create function to get faclity name for popups
+
+        function getPopups() {
+            var popup = [];
+
+            for (var i = 0; i < response.length; i += 1) {
+
+                if (location) {
+                    var name = response[i].FACILITY_NAME
+                    popup.push(name);
+
+                }
+            }
+            // console.log(popup)
+            return popup;
+        }
+
+        test.bindPopup(getPopups()).openPopup();
+
+
+    });
+
+
+    //Set up health distric boundries
+    var link = "static/data/hd.geojson"
+
+    d3.json(link, function(data) {
+        console.log(data)
+        health_districts = L.geoJson(data, {
+                style: function(feature) {
+                    return {
+                        color: "black",
+                        //fillColor: "blue", //chooseColor(feature.properties.borough),
+                        fillOpacity: .2,
+                        weight: 2
+                    }
+                },
+                onEachFeature: function(feature, layer) {
+                    layer.on({
+                        mouseover: function(event) {
+                            layer = event.target
+                            layer.setStyle({
+                                fillOpacity: .8
+                            });
+                        },
+                        mouseout: function(event) {
+                                layer = event.target
+                                layer.setStyle({
+                                    fillOpacity: .2
+                                })
+                            }
+                            // ,
+                            // click: function(event) {
+                            //     myMap.fitBounds(event.target.getBounds())
+                            // }
+                    });
+                    layer.bindPopup("<h1>" + feature.properties.hd_name + " (" + feature.properties.hd_2012 + ")" + "</h1>")
+                }
+            }) //.addTo(myMap)
+
+        // Sending income, districts, and spa layer to the createMap function
+        createMap(income, health_districts, spa, test);
+    });
+
+});
+
+
+
+// var map = L.map("map"),
+//   parentGroup = L.markerClusterGroup(options), // Could be any other Layer Group type.
+//   // This is where the magic happens!
+//   mySubGroup = L.featureGroup.subGroup(parentGroup, arrayOfMarkers);
+
+// parentGroup.addTo(map);
+// mySubGroup.addTo(map);
+
+
+
+function createMap(income, health_districts, spa, test) {
+
+    // Create tile layer
+    var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+        tileSize: 512,
+        maxZoom: 18,
+        zoomOffset: -1,
+        id: "mapbox/streets-v11",
+        accessToken: API_KEY
+    })
+
+    var darktmap = L.tileLayer('http://tile.stamen.com/toner/{z}/{x}/{y}.png', {
+            attribution: 'Stamen'
+        }) //.addTo(map);
+
+    //Experment
+    var interaction = cartodb.createLayer(map, 'http://documentation.cartodb.com/api/v2/viz/2b13c956-e7c1-11e2-806b-5404a6a683d5/viz.json')
+        .addTo(map)
+        .on('done', function(layer) {
+
+            layer.setInteraction(true);
+
+            layer.on('featureOver', function(e, latlng, pos, data) {
+                cartodb.log.log(e, latlng, pos, data);
+            });
+
+            layer.on('error', function(err) {
+                cartodb.log.log('error: ' + err);
+            });
+        }).on('error', function() {
+            cartodb.log.log("some error occurred");
+        });
+
+    // Create a baseMaps object to hold the lightmap layer
+    var baseMaps = {
+        "Ligh map": lightmap,
+        "Dark map": darktmap
+    };
+
+    // Create an overlays object to add to the layer control
+    var overlays = {
+        "Income": income,
+        "Health Districts": health_districts,
+        "Service Planning Area": spa,
+        "Hospitals": test
+    };
+
+    // Create the map with our layers
+    var myMap = L.map("map", {
+        center: [34.26, -118.243683],
+        zoom: 9.5,
+        layers: [
+            lightmap,
+            income,
+            health_districts,
+            spa,
+            test
+        ]
+    });
+
+
+    // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
+    L.control.layers(baseMaps, overlays, {
+        collapsed: false
+    }).addTo(myMap);
+
+    // Set up the legend
+    var legend = L.control({ position: "bottomright" });
+    legend.onAdd = function() {
+        var div = L.DomUtil.create("div", "info legend");
+        var limits = income.options.limits;
+        var colors = income.options.colors;
+        var labels = [];
+
+        // Add min & max
+        var legendInfo = "<h1>Median Income</h1>" +
+            "<div class=\"labels\">" +
+            "<div class=\"min\">" + limits[0] + "</div>" +
+            "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+            "</div>";
+
+        div.innerHTML = legendInfo;
+
+        limits.forEach(function(limit, index) {
+            labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+        });
+
+        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+        return div;
+    };
+
+    // Adding legend to the map
+    legend.addTo(myMap);
+}
